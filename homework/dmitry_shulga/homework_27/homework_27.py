91a6be4bb994695b4b2ccb4321f1f173074c0985
Dmitry Shulga <Shulga.dmitry.16.1992@yandex.by>
2022-11-07 16:23:03 +0300

    Homework_27 changed

--- homework/dmitry_shulga/homework_27/homework_27.py
+++ homework/dmitry_shulga/homework_27/homework_27.py
@@ -12,23 +12,22 @@ cursor = db.cursor(dictionary=True)
 cursor.execute("INSERT INTO `groups` (title, start_date, end_date) VALUES ('QAP_09Oonl', '2022-11-06', '2023-11-06')")
 db.commit()
 group_id = cursor.lastrowid
-print(group_id)
 cursor.execute(f"INSERT INTO students (name, second_name, group_id) VALUES ('Jack', 'Sparrow', {group_id})")
 db.commit()
 student_id = cursor.lastrowid
-print(student_id)
 query = "INSERT INTO books (title, taken_by_student_id) VALUES (%s, %s)"
 values = [
-    ('The Green Mile', f'{student_id}'),
-    ('The Shawshank Redemption', f'{student_id}')
+    ('The Green Mile', student_id),
+    ('The Shawshank Redemption', student_id)
 ]
 cursor.executemany(query, values)
 db.commit()
 cursor.execute(f'SELECT s.name, s.second_name, g.title as group_title, b.title as book_title FROM students s '
                f'JOIN `groups` g ON s.group_id = g.id JOIN books b ON b.taken_by_student_id = s.id '
                f'WHERE s.id = {student_id}')
-result_1 = cursor.fetchone()
-result_2 = cursor.fetchone()
-print(f"Student {result_1['name']} {result_1['second_name']} studies in the {result_1['group_title']} group and "
-      f"borrowed the following books from the library: {result_1['book_title']}, {result_2['book_title']}")
+#result_1 = cursor.fetchone()
+#result_2 = cursor.fetchone()
+result = cursor.fetchall()
+print(f"Student {result[0]['name']} {result[0]['second_name']} studies in the {result[0]['group_title']} group and "
+      f"borrowed the following books from the library: {result[0]['book_title']}, {result[0]['book_title']}")
 db.close()
